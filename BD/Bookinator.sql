a5bd9e0400067eaf08ed454ab2e69e467647865d
main0007 <thibaut.maingot@etudiant.univ-reims.fr>
2021-06-04 11:09:14 +0200

    Modif du script sql pour MYSQL, certaines colonnes ne sont plus NOT NULL

--- BD/Bookinator.sql
+++ BD/Bookinator.sql
@@ -1,262 +1,274 @@
-CREATE SEQUENCE COUVERTURE_IDCOUV_SEQ;
 
 CREATE TABLE Couverture (
-                idCouv NUMBER NOT NULL,
-                png BLOB NOT NULL,
-                CONSTRAINT COUVERTURE_PK PRIMARY KEY (idCouv)
+                idCouv INT AUTO_INCREMENT NOT NULL,
+                png LONGBLOB NOT NULL,
+                PRIMARY KEY (idCouv)
 );
 
-
-CREATE SEQUENCE UTILISATEUR_IDUTILISATEUR_SEQ;
 
 CREATE TABLE Utilisateur (
-                idUtilisateur NUMBER NOT NULL,
-                nom VARCHAR2(30) NOT NULL,
-                prnm VARCHAR2(30) NOT NULL,
+                idUtilisateur INT AUTO_INCREMENT NOT NULL,
+                nom VARCHAR(30) NOT NULL,
+                prnm VARCHAR(30) NOT NULL,
                 dateNais DATE NOT NULL,
-                tel CHAR(10) NOT NULL,
-                mail VARCHAR2(50) NOT NULL,
-                ville VARCHAR2(38) NOT NULL,
-                CP CHAR(5) NOT NULL,
-                rue VARCHAR2(40) NOT NULL,
-                pseudo VARCHAR2(30),
-                mdp VARCHAR2(123) NOT NULL,
-                photoProfil BLOB,
-                isAdmin NUMBER DEFAULT 0 NOT NULL,
-                CONSTRAINT PK_UTILISATEUR PRIMARY KEY (idUtilisateur),
-                CONSTRAINT CK_ISADMIN CHECK (isAdmin BETWEEN 0 AND 1)
+                tel CHAR(10),
+                mail VARCHAR(50) NOT NULL,
+                ville VARCHAR(38),
+                CP CHAR(5),
+                rue VARCHAR(40),
+                pseudo VARCHAR(30),
+                mdp VARCHAR(123) NOT NULL,
+                photoProfil LONGBLOB,
+                isAdmin INT DEFAULT 0 NOT NULL,
+                PRIMARY KEY (idUtilisateur)
 );
 
-
-CREATE SEQUENCE SIGNALEMENT_IDSIGNALEMENT_SEQ;
 
 CREATE TABLE Signalement (
-                idSignalement NUMBER NOT NULL,
-                contenu VARCHAR2(500) NOT NULL,
+                idSignalement INT AUTO_INCREMENT NOT NULL,
+                contenu VARCHAR(500) NOT NULL,
                 dateSig DATE NOT NULL,
-                idUtilisateur NUMBER NOT NULL,
-                CONSTRAINT PK_SIGNALEMENT PRIMARY KEY (idSignalement)
+                idUtilisateur INT NOT NULL,
+                PRIMARY KEY (idSignalement)
 );
 
-
-CREATE SEQUENCE STATUSCOMMANDE_IDSTATUS_SEQ;
 
 CREATE TABLE StatusCommande (
-                idStatus NUMBER NOT NULL,
-                libStatus VARCHAR2(30) NOT NULL,
-                CONSTRAINT PK_STATUSCOMMANDE PRIMARY KEY (idStatus)
+                idStatus INT AUTO_INCREMENT NOT NULL,
+                libStatus VARCHAR(30) NOT NULL,
+                PRIMARY KEY (idStatus)
 );
 
-
-CREATE SEQUENCE COMMANDE_IDCMD_SEQ;
 
 CREATE TABLE Commande (
-                idCmd NUMBER NOT NULL,
-                idUtilisateur NUMBER NOT NULL,
-                idStatus NUMBER NOT NULL,
-                prixCmd FLOAT NOT NULL,
+                idCmd INT AUTO_INCREMENT NOT NULL,
+                idUtilisateur INT NOT NULL,
+                idStatus INT NOT NULL,
+                prixCmd DOUBLE NOT NULL,
                 dateCmd DATE NOT NULL,
-                villeLivraison VARCHAR2(38) NOT NULL,
-                CPLivraison VARCHAR2(30) NOT NULL,
-                rueLivraison VARCHAR2(30) NOT NULL,
-                CONSTRAINT PK_COMMANDE PRIMARY KEY (idCmd)
+                villeLivraison VARCHAR(38) NOT NULL,
+                CPLivraison VARCHAR(30) NOT NULL,
+                rueLivraison VARCHAR(30) NOT NULL,
+                PRIMARY KEY (idCmd)
 );
 
-
-CREATE SEQUENCE AUTEUR_IDAUTEUR_SEQ;
 
 CREATE TABLE Auteur (
-                idAuteur NUMBER NOT NULL,
-                nom VARCHAR2(30) NOT NULL,
-                prnm VARCHAR2(30) NOT NULL,
+                idAuteur INT AUTO_INCREMENT NOT NULL,
+                nom VARCHAR(30) NOT NULL,
+                prnm VARCHAR(30) NOT NULL,
                 dateNais DATE NOT NULL,
-                CONSTRAINT PK_AUTEUR PRIMARY KEY (idAuteur)
+                PRIMARY KEY (idAuteur)
 );
 
-
-CREATE SEQUENCE SUPPORT_IDSUPPORT_SEQ;
 
 CREATE TABLE Support (
-                idSupport NUMBER NOT NULL,
-                libSupport VARCHAR2(30) NOT NULL,
-                CONSTRAINT PK_SUPPORT PRIMARY KEY (idSupport)
+                idSupport INT AUTO_INCREMENT NOT NULL,
+                libSupport VARCHAR(30) NOT NULL,
+                PRIMARY KEY (idSupport)
 );
 
-
-CREATE SEQUENCE FORMAT_IDFORMAT_SEQ;
 
 CREATE TABLE Format (
-                idFormat NUMBER NOT NULL,
-                libFormat VARCHAR2(30) NOT NULL,
-                largeur FLOAT NOT NULL,
-                hauteur FLOAT NOT NULL,
-                CONSTRAINT PK_FORMAT PRIMARY KEY (idFormat)
+                idFormat INT AUTO_INCREMENT NOT NULL,
+                libFormat VARCHAR(30) NOT NULL,
+                largeur DOUBLE NOT NULL,
+                hauteur DOUBLE NOT NULL,
+                PRIMARY KEY (idFormat)
 );
 
-
-CREATE SEQUENCE GENRE_IDGENRE_SEQ;
 
 CREATE TABLE Genre (
-                idGenre NUMBER NOT NULL,
-                libGenre VARCHAR2(30) NOT NULL,
-                CONSTRAINT PK_GENRE PRIMARY KEY (idGenre)
+                idGenre INT AUTO_INCREMENT NOT NULL,
+                libGenre VARCHAR(30) NOT NULL,
+                PRIMARY KEY (idGenre)
 );
 
 
-CREATE SEQUENCE EDITEUR_IDEDITEUR_SEQ;
-
 CREATE TABLE Editeur (
-                idEditeur NUMBER NOT NULL,
-                libEditeur VARCHAR2(30) NOT NULL,
-                CONSTRAINT PK_EDITEUR PRIMARY KEY (idEditeur)
+                idEditeur INT AUTO_INCREMENT NOT NULL,
+                libEditeur VARCHAR(30) NOT NULL,
+                PRIMARY KEY (idEditeur)
 );
 
 
 CREATE TABLE Livre (
-                ISBN VARCHAR2(13) NOT NULL,
-                titre VARCHAR2(30) NOT NULL,
+                ISBN VARCHAR(13) NOT NULL,
+                titre VARCHAR(30) NOT NULL,
                 datePublication DATE NOT NULL,
-                nbPages NUMBER NOT NULL,
-                langue VARCHAR2(30) NOT NULL,
-                description LONG NOT NULL,
-                prix FLOAT NOT NULL,
-                qte NUMBER NOT NULL,
-                idEditeur NUMBER NOT NULL,
-                idCouv NUMBER NOT NULL,
-                idFormat NUMBER NOT NULL,
-                idSupport NUMBER NOT NULL,
-                CONSTRAINT PK_LIVRE PRIMARY KEY (ISBN)
+                nbPages INT NOT NULL,
+                langue VARCHAR(30) NOT NULL,
+                description TEXT NOT NULL,
+                prix DOUBLE NOT NULL,
+                qte INT NOT NULL,
+                idEditeur INT NOT NULL,
+                idCouv INT NOT NULL,
+                idFormat INT NOT NULL,
+                idSupport INT NOT NULL,
+                PRIMARY KEY (ISBN)
 );
 
 
 CREATE TABLE Favoris (
-                ISBN VARCHAR2(13) NOT NULL,
-                idUtilisateur NUMBER NOT NULL,
-                CONSTRAINT PK_FAVORIS PRIMARY KEY (ISBN, idUtilisateur)
+                ISBN VARCHAR(13) NOT NULL,
+                idUtilisateur INT NOT NULL,
+                PRIMARY KEY (ISBN, idUtilisateur)
 );
 
 
 CREATE TABLE Contenu (
-                idCmd NUMBER NOT NULL,
-                ISBN VARCHAR2(13) NOT NULL,
-                CONSTRAINT PK_CONTENU PRIMARY KEY (idCmd, ISBN)
+                idCmd INT NOT NULL,
+                ISBN VARCHAR(13) NOT NULL,
+                PRIMARY KEY (idCmd, ISBN)
 );
 
 
 CREATE TABLE Ecrire (
-                idAuteur NUMBER NOT NULL,
-                ISBN VARCHAR2(13) NOT NULL,
-                CONSTRAINT PK_ECRIRE PRIMARY KEY (idAuteur, ISBN)
+                idAuteur INT NOT NULL,
+                ISBN VARCHAR(13) NOT NULL,
+                PRIMARY KEY (idAuteur, ISBN)
 );
 
 
 CREATE TABLE AvoirGenre (
-                idGenre NUMBER NOT NULL,
-                ISBN VARCHAR2(13) NOT NULL,
-                CONSTRAINT PK_AVOIRGENRE PRIMARY KEY (idGenre, ISBN)
+                idGenre INT NOT NULL,
+                ISBN VARCHAR(13) NOT NULL,
+                PRIMARY KEY (idGenre, ISBN)
 );
 
-
-CREATE SEQUENCE APPRECIATION_IDAPPRECIATION_SEQ;
 
 CREATE TABLE Appreciation (
-                idAppreciation NUMBER NOT NULL,
-                commentaire VARCHAR2(500) NOT NULL,
-                note NUMBER NOT NULL,
+                idAppreciation INT AUTO_INCREMENT NOT NULL,
+                commentaire VARCHAR(500) NOT NULL,
+                note INT NOT NULL,
                 dateApp DATE NOT NULL,
-                idUtilisateur NUMBER NOT NULL,
-                ISBN VARCHAR2(13) NOT NULL,
-                CONSTRAINT PK_APPRECIATION PRIMARY KEY (idAppreciation)
+                idUtilisateur INT NOT NULL,
+                ISBN VARCHAR(13) NOT NULL,
+                PRIMARY KEY (idAppreciation)
 );
 
 
-ALTER TABLE Livre ADD CONSTRAINT FK_COUVERTURE_LIVRE
+ALTER TABLE Livre ADD CONSTRAINT couverture_livre_fk
 FOREIGN KEY (idCouv)
-REFERENCES Couverture (idCouv);
+REFERENCES Couverture (idCouv)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Commande ADD CONSTRAINT FK_UTILISATEUR_COMMANDE
+ALTER TABLE Commande ADD CONSTRAINT utilisateur_commande_fk
 FOREIGN KEY (idUtilisateur)
-REFERENCES Utilisateur (idUtilisateur);
+REFERENCES Utilisateur (idUtilisateur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Appreciation ADD CONSTRAINT FK_UTILISATEUR_APPRECIATION
+ALTER TABLE Appreciation ADD CONSTRAINT utilisateur_appreciation_fk
 FOREIGN KEY (idUtilisateur)
-REFERENCES Utilisateur (idUtilisateur);
+REFERENCES Utilisateur (idUtilisateur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Favoris ADD CONSTRAINT FK_UTILISATEUR_FAVORIS
+ALTER TABLE Favoris ADD CONSTRAINT utilisateur_favoris_fk
 FOREIGN KEY (idUtilisateur)
-REFERENCES Utilisateur (idUtilisateur);
+REFERENCES Utilisateur (idUtilisateur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Signalement ADD CONSTRAINT FK_UTILISATEUR_SIGNALEMENT
+ALTER TABLE Signalement ADD CONSTRAINT utilisateur_signalement_fk
 FOREIGN KEY (idUtilisateur)
-REFERENCES Utilisateur (idUtilisateur);
+REFERENCES Utilisateur (idUtilisateur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Commande ADD CONSTRAINT FK_STATUSCOMMANDE_COMMANDE
+ALTER TABLE Commande ADD CONSTRAINT statuscommande_commande_fk
 FOREIGN KEY (idStatus)
-REFERENCES StatusCommande (idStatus);
+REFERENCES StatusCommande (idStatus)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Contenu ADD CONSTRAINT FK_COMMANDE_CONTENU
+ALTER TABLE Contenu ADD CONSTRAINT commande_contenu_fk
 FOREIGN KEY (idCmd)
-REFERENCES Commande (idCmd);
+REFERENCES Commande (idCmd)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Ecrire ADD CONSTRAINT FK_AUTEUR_ECRIRE
+ALTER TABLE Ecrire ADD CONSTRAINT auteur_ecrire_fk
 FOREIGN KEY (idAuteur)
-REFERENCES Auteur (idAuteur);
+REFERENCES Auteur (idAuteur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Livre ADD CONSTRAINT FK_SUPPORT_LIVRE
+ALTER TABLE Livre ADD CONSTRAINT support_livre_fk
 FOREIGN KEY (idSupport)
-REFERENCES Support (idSupport);
+REFERENCES Support (idSupport)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Livre ADD CONSTRAINT FK_FORMAT_LIVRE
+ALTER TABLE Livre ADD CONSTRAINT format_livre_fk
 FOREIGN KEY (idFormat)
-REFERENCES Format (idFormat);
+REFERENCES Format (idFormat)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE AvoirGenre ADD CONSTRAINT FK_GENRE_AVOIRGENRE
+ALTER TABLE AvoirGenre ADD CONSTRAINT genre_avoirgenre_fk
 FOREIGN KEY (idGenre)
-REFERENCES Genre (idGenre);
+REFERENCES Genre (idGenre)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Livre ADD CONSTRAINT FK_EDITEUR_LIVRE
+ALTER TABLE Livre ADD CONSTRAINT editeur_livre_fk
 FOREIGN KEY (idEditeur)
-REFERENCES Editeur (idEditeur);
+REFERENCES Editeur (idEditeur)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Appreciation ADD CONSTRAINT FK_LIVRE_APPRECIATION
+ALTER TABLE Appreciation ADD CONSTRAINT livre_appreciation_fk
 FOREIGN KEY (ISBN)
-REFERENCES Livre (ISBN);
+REFERENCES Livre (ISBN)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE AvoirGenre ADD CONSTRAINT FK_LIVRE_AVOIRGENRE
+ALTER TABLE AvoirGenre ADD CONSTRAINT livre_avoirgenre_fk
 FOREIGN KEY (ISBN)
-REFERENCES Livre (ISBN);
+REFERENCES Livre (ISBN)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Ecrire ADD CONSTRAINT FK_LIVRE_ECRIRE
+ALTER TABLE Ecrire ADD CONSTRAINT livre_ecrire_fk
 FOREIGN KEY (ISBN)
-REFERENCES Livre (ISBN);
+REFERENCES Livre (ISBN)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Contenu ADD CONSTRAINT FK_LIVRE_CONTENU
+ALTER TABLE Contenu ADD CONSTRAINT livre_contenu_fk
 FOREIGN KEY (ISBN)
-REFERENCES Livre (ISBN);
+REFERENCES Livre (ISBN)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
-ALTER TABLE Favoris ADD CONSTRAINT FK_LIVRE_FAVORIS
+ALTER TABLE Favoris ADD CONSTRAINT livre_favoris_fk
 FOREIGN KEY (ISBN)
-REFERENCES Livre (ISBN);
+REFERENCES Livre (ISBN)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
 
 CREATE TABLE  SignalementCommentaire(     
-                 idSignalement NUMBER NOT NULL,
-                 idAppreciation NUMBER NOT NULL,
+                 idSignalement INT NOT NULL,
+                 idAppreciation INT NOT NULL,
                  CONSTRAINT PK_IDSIGNALEMENT PRIMARY KEY (idSignalement),
                  CONSTRAINT FK_SIGNALEMENTCOMMENTAIRE_HERITAGE__SIGNALEMENT FOREIGN KEY (idSignalement) REFERENCES SIGNALEMENT (idSignalement),
                  CONSTRAINT FK_IDAPPRECIATION FOREIGN KEY (idAppreciation) REFERENCES Appreciation(idAppreciation)
 );
 
 CREATE TABLE  SignalementLivre(     
-                 idSignalement NUMBER NOT NULL,
-                 idLivre NUMBER NOT NULL,
+                 idSignalement INT NOT NULL,
+                 idLivre INT NOT NULL,
                  CONSTRAINT PK_IDSIGNALEMENT PRIMARY KEY (idSignalement),
                  CONSTRAINT FK_SIGNALEMENTLIVRE_HERITAGE__SIGNALEMENT FOREIGN KEY (idSignalement) REFERENCES SIGNALEMENT (idSignalement),
                  CONSTRAINT FK_IDLIVRE FOREIGN KEY (idLivre) REFERENCES Livre(idLivre)
 );
 
 CREATE TABLE  SignalementCommande(     
-                 idSignalement NUMBER NOT NULL,
-                 idCommande NUMBER NOT NULL,
+                 idSignalement INT NOT NULL,
+                 idCommande INT NOT NULL,
                  CONSTRAINT PK_IDSIGNALEMENT PRIMARY KEY (idSignalement),
                  CONSTRAINT FK_SIGNALEMENTCOMMANDE_HERITAGE__SIGNALEMENT FOREIGN KEY (idSignalement) REFERENCES SIGNALEMENT (idSignalement),
                  CONSTRAINT FK_IDCOMMANDE FOREIGN KEY (idCommande) REFERENCES Commande(idCommande)
