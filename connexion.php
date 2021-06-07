3e092eaba31cdc678e74078d62fc69d57fdec7e7
FOSSIER Lucie <lucie.fossier@etudiant.univ-reims.fr>
2021-06-07 17:07:51 +0200

    ajout de la flèche dans connexion.php

--- connexion.php
+++ connexion.php
@@ -11,11 +11,26 @@ $webPage->appendCssUrl("src/style.css");
 
 $form = <<<HTML
 
-         <a href="index.php" class="font-size-24 main-color-background dark-text border-radius-5 padding-button font-weight-bold button">
-            <svg width="52" height="43" viewBox="0 0 52 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 15.6667H36.2667C39.3785 15.6667 42.363 16.8958 44.5634 19.0838C46.7638 21.2717 48 24.2391 48 27.3333C48 30.4275 46.7638 33.395 44.5634 35.5829C42.363 37.7708 39.3785 39 36.2667 39H33.3333M15.7333 27.3333L4 15.6667L15.7333 4V27.3333Z" stroke="#2F2F2F" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </a>
+
+         <a href="index.php" class="d-flex">
+                <svg width="60" height="60" viewBox="0 0 108 108" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_d)">
+                        <rect x="4" width="100" height="100" rx="50" fill="#E1B74A"/>
+                    </g>
+                    <path d="M32 43.6667H64.2667C67.3785 43.6667 70.363 44.8958 72.5634 47.0838C74.7638 49.2717 76 52.2391 76 55.3333C76 58.4275 74.7638 61.395 72.5634 63.5829C70.363 65.7708 67.3785 67 64.2667 67H61.3333M43.7333 55.3333L32 43.6667L43.7333 32V55.3333Z" stroke="#2F2F2F" stroke-width="7" stroke-linecap="round" stroke-linejoin="round"/>
+                    <defs>
+                        <filter id="filter0_d" x="0" y="0" width="108" height="108" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                            <feOffset dy="4"/>
+                            <feGaussianBlur stdDeviation="2"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape"/>
+                        </filter>
+                    </defs>
+                </svg>
+            </a>
 
         <div class="d-flex m-5 p-5 main-background flex-column align-items-center border-radius-10 login-form justify-content-center ">
         
@@ -36,6 +51,7 @@ $form = <<<HTML
             
             <a href="inscription.php" class="booki-link">Pas encore de Compte ?</a>
         </div>
+
     
 HTML;
 
