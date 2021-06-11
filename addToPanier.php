c78d876c5dcebdb92b54634f5f0c4d07a97e9634
main0007 <thibaut.maingot@etudiant.univ-reims.fr>
2021-06-11 09:49:00 +0200

    Correctif addToPanier.php (erreur non connecte et cle primaire)

--- addToPanier.php
+++ addToPanier.php
@@ -5,10 +5,14 @@ require_once "autoload.php";
 require_once "src/Utils.php";
 
 init_php_session();
+if(!isLogged())
+    header('Location: index.php');
 
 $id='';
 if(isset($_GET['id']) && !empty($_GET['id']) && ctype_digit($_GET['id']))
     $id=$_GET['id'];
+else
+    header('Location: index.php');
 
 Utilisateur::createFromId($_SESSION['idUtilisateur'])->getPanier()->addContenu($id);
 header('Location: Article.php?idArticle='.$id);
